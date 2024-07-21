0347b2f9e1706316cad3e64cda27a93ea7395ae4
2131730057@student.polinema.ac.id <2131730057@student.polinema.ac.id>
2024-07-21 13:40:29 +0700

    okeii

--- application/controllers/api/Manage_all.php
+++ application/controllers/api/Manage_all.php
@@ -682,7 +682,7 @@ class Manage_all extends RestController
             "status" => $this->post('status'),
             "status_bayar" => $this->post('status_bayar')
         ];
-        $this->response($data);
+        $this->response($id);
         $this->db->where('id', $id);
         $this->db->update('transaksi', $data);
 
